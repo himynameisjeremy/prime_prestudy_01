09928b7bff9413d754a762a9fb02e18755653de8
himynameisjeremy <himynameisjeremy@gmail.com>
2016-02-14 20:44:10 -0600

    first commit

--- function.js
+++
@@ -1,7 +0,0 @@
-var x=prompt ("How old are you?")
-function time (age) {
-	var months=age*12
-	alert("Congrats! You managed to survive " + months + " months! And even more amazing, this function works!");
-	console.log("Hey this works!")
-}
-time (x)
